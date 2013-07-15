dae3b18e6715da8f8cde016b2bd51418d377d5b7
Mykhailo Kotsur <miccots@gmail.com>
2013-07-15 19:00:31 +0200

    Modified script for deployit update

--- newDeployit.sh
+++ newDeployit.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-filename=$(basename "$2")
+currentPath=${1%/}
+distPath="$2"
+
+if [[ -d "$2" ]]
+    then
+        [[ ! -d "$2/cli" || ! -d "$2/server" ]] && echo "$2 does not seem to be Deployed source dir" && exit 10
+        cd "$2" && gradle buildRelease
+        f=`find . -name "deployit-*-server.zip"`
+        echo "Found new deployit server: ${f}"
+        distPath="$2/$f"
+fi
+
+filename=$(basename "$distPath")
 extension="${filename##*.}"
 filename="${filename%.*}"
 
@@ -10,18 +22,16 @@ then
     exit 10;
 fi
 
+echo "Replacing ${currentPath} with ${distPath}"
 
-echo "Replacing ${1} with ${2}"
-
-
-rootPath=`dirname "${1}"`
-oldPath="${1}_old"
+rootPath=`dirname "${currentPath}"`
+oldPath="${currentPath}_old"
 
 rm -rf ${oldPath}
 
-mv "${1}" "${oldPath}"
+mv "${currentPath}" "${oldPath}"
 
-unzip "${2}" -d "${rootPath}"
+unzip "${distPath}" -d "${rootPath}"
 
 newPath="${rootPath}/${filename}" 
 
